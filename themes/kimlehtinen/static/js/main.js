2f6c095204963c92e91802fd14136db1c04bcd28
Kim Lehtinen <kim@kimlehtinen.com>
2024-10-25 23:45:51 +0300

    Make slideshow gallery work when multiple slideshows on same page

--- themes/kimlehtinen/static/js/main.js
+++ themes/kimlehtinen/static/js/main.js
@@ -1,40 +1,71 @@
 function onNavbarHamburgerMenuClick() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
 
 /**
- * Image Slideshow Gallery
- */
-let imgshGaIndex = 1;
-imgshGaShowSlides(imgshGaIndex);
+* Image Slideshow Gallery
+*/
+document.addEventListener("DOMContentLoaded", function() {
+let slideshows = document.querySelectorAll(".img-slideshow-gallery-container");
 
-function plusSlides(n) {
-  imgshGaShowSlides(imgshGaIndex += n);
+slideshows.forEach((slideshow, index) => {
+  let imgshGaIndex = 1;
+  imgshGaShowSlides(slideshow, imgshGaIndex);
+
+  slideshow.querySelector(".img-slideshow-gallery-prev").addEventListener("click", function() {
+    plusSlides(slideshow, -1);
+  });
+
+  slideshow.querySelector(".img-slideshow-gallery-next").addEventListener("click", function() {
+    plusSlides(slideshow, 1);
+  });
+
+  let dots = slideshow.querySelectorAll(".img-slideshow-gallery-demo");
+  dots.forEach((dot, dotIndex) => {
+    dot.addEventListener("click", function() {
+      currentSlide(slideshow, dotIndex + 1);
+    });
+  });
+});
+
+function plusSlides(slideshow, n) {
+  let imgshGaIndex = parseInt(slideshow.getAttribute("data-index")) || 1;
+  imgshGaIndex += n;
+  if (imgshGaIndex > slideshow.getElementsByClassName("img-slideshow-gallery-myslides").length) {
+    imgshGaIndex = 1;
+  }
+  if (imgshGaIndex < 1) {
+    imgshGaIndex = slideshow.getElementsByClassName("img-slideshow-gallery-myslides").length;
+  }
+  slideshow.setAttribute("data-index", imgshGaIndex);
+  imgshGaShowSlides(slideshow, imgshGaIndex);
 }
 
-function currentSlide(n) {
-  imgshGaShowSlides(imgshGaIndex = n);
+function currentSlide(slideshow, n) {
+  slideshow.setAttribute("data-index", n);
+  imgshGaShowSlides(slideshow, n);
 }
 
-function imgshGaShowSlides(n) {
+function imgshGaShowSlides(slideshow, n) {
   let i;
-  let slides = document.getElementsByClassName("img-slideshow-gallery-myslides");
-  let dots = document.getElementsByClassName("img-slideshow-gallery-demo");
-  let captionText = document.getElementById("img-slideshow-gallery-caption");
-  if (n > slides.length) {imgshGaIndex = 1}
-  if (n < 1) {imgshGaIndex = slides.length}
+  let slides = slideshow.getElementsByClassName("img-slideshow-gallery-myslides");
+  let dots = slideshow.getElementsByClassName("img-slideshow-gallery-demo");
+  let captionText = slideshow.querySelector(".img-slideshow-gallery-caption");
+  if (n > slides.length) { n = 1 }
+  if (n < 1) { n = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[imgshGaIndex-1].style.display = "block";
-  dots[imgshGaIndex-1].className += " active";
-  captionText.innerHTML = dots[imgshGaIndex-1].alt;
+  slides[n - 1].style.display = "block";
+  dots[n - 1].className += " active";
+  captionText.innerHTML = dots[n - 1].alt;
 }
+});
